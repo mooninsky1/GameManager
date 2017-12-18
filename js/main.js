58ca97b4980081c2681db2d20fc17949cc1be469
mooninsky1 <moon_in_sky@sina.cn>
2017-12-18 18:16:51 +0800

    整理node.js网面跳转

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 var fs = require('fs');
 var http = require('http');
 var server = http.createServer();
+var path = require('path');
 server.on('request',function(req,res)
 {
-    res.writeHead(200,{'Content-Type':'text/html'});
-    var rs = fs.createReadStream('../html/ossmanu.html');
-    rs.pipe(res);
+    console.log(req.url);
+    switch(req.url)
+    {
+        case '/':
+        {
+            res.writeHead(200,{'Content-Type':'text/html'});
+            var rs = fs.createReadStream('../html/ossmanu.html');
+            rs.pipe(res);
+            break;
+        }
+       default:
+       {    
+           if(path.extname(req.url) == '.html')
+           {
+                res.writeHead(200,{'Content-Type':'text/html'});
+                var rs = fs.createReadStream("../html/" +req.url);
+                rs.pipe(res);
+           }
+           else
+           {
+               console.log("not prcess url "+req.url);
+           }
+        }
+    }
+    
+  
 
 });
 server.listen(4000);
